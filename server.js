4b686b6d9e68c321321436275ee66ff9efc497c2
rishishal <rishisahilcomputer@gmail.com>
2023-05-28 12:13:08 -0700

    Script Update

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ connectDB();
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
-//Cors
+// Cors
 const corsOptions = {
   origin: process.env.ALLOWED_CLIENTS.split(","),
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 };
 
 app.use(cors(corsOptions));
