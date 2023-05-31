1887a80b2c7e2a9d27731ca9e761460a80fb2929
rishishal <rishisahilcomputer@gmail.com>
2023-06-01 00:37:39 +0530

    port

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const connectDB = require("./config/db");
-const { connect } = require("mongoose");
-const path = require("path");
-connectDB();
-const cors = require("cors");
 const PORT = process.env.PORT || 3000;
-
+const path = require("path");
+const cors = require("cors");
 // Cors
 const corsOptions = {
   origin: process.env.ALLOWED_CLIENTS.split(","),
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 };
 
-app.use(cors(corsOptions));
+// Default configuration looks like
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }
 
+app.use(cors(corsOptions));
 app.use(express.static("public"));
+
+const connectDB = require("./config/db");
+connectDB();
+
 app.use(express.json());
-//Template engine
+
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
-//Routes
+
+// Routes
 app.use("/api/files", require("./routes/files"));
 app.use("/files", require("./routes/show"));
 app.use("/files/download", require("./routes/download"));
